8b01d05611837dd553ae32ae3a6a7ff5fca7a201
CosmoOrSth <79050675+CosmoOrSth@users.noreply.github.com>
2025-04-20 18:54:24 +0530

    Initial commit

---
+++ src/client/java/com/smithy/darcevo/DarcEvolutionClient.java
@@ -0,0 +1,10 @@
+package com.smithy.darcevo;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DarcEvolutionClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
